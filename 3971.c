3cc4b7b387a2dc7033a679d4bebc50c5bdc605ac
kkaaii <zhaokai115@163.com>
2016-11-14 11:27:31 +0800

    Create 3971.c

---
+++ 3971.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+#define N   1000000
+
+char w[N + 1];
+int n;
+int d;
+
+int dfs(void);
+
+int main(void)
+{
+    int cases;
+    int b;
+    
+    scanf("%d", &cases);
+    while (cases--) {
+        memset(w, '0', sizeof (w));
+        scanf("%d%d%d\n", &n, &b, &d);
+        scanf("%s", &w[n - b]);
+        printf("%d\n", dfs());
+    }
+
+    return 0;
+}
+
+int dp[2];
+
+int dfs(void)
+{
+    int i;
+    int j;
+    
+    dp[0] = 1;
+    dp[1] = 0;
+    
+    for (i = 1; i <= n; ++i) {
+        j = w[n - i] - '0';
+        dp[j] = (dp[0] + dp[1]) % d;
+    }
+    
+    return dp[0];
+}
