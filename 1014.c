1c5c54138105711fbd4dda1a870dd1ac01eb6e45
kkaaii <zhaokai115@163.com>
2016-12-06 20:49:07 +0800

    Update 1014.c

--- 1014.c
+++ 1014.c
@@ -5,26 +5,27 @@
 #define N   20000
 
 int m[M + 1];
+int sum;
 
 static int dfs(void);
 
 static int input(void)
 {
-    int total = 0;
     int i;
 
+    sum = 0;
     for (i = 1; i <= M; ++i) {
         scanf("%d", &m[i]);
-        total += m[i];
+        sum += i * m[i];
     }
 
-    return total;
+    return sum;
 }
 
 int main(void)
 {
     int i;
-    
+
     for (i = 1; input(); ++i) {
         printf("Collection #%d:\n", i);
         puts(dfs() ? "Can be divided.\n" : "Can't be divided.\n");
@@ -38,25 +39,37 @@ int dp[N * M + 1];
 
 int dfs(void)
 {
-    int c;
-    int sum;
-    
-    sum = 0;
-    for (c = 1; c <= M; ++c) {
-        sum += c * m[c];
-    }
+    int i, j, k;
 
     if (sum & 1) {
         return 0;
     }
 
-    memset(dp, 0x80, sizeof (dp));
-    dp[0] = 0;
+    memset(dp, 0, sizeof (dp));
+    dp[0] = 1;
+    sum /= 2;
+    v = 0;
 
-    v = sum / 2;
-    for (c = 1; c <= M; ++c) {
-        /* TODO */
+    for (i = 1; i <= M; ++i) {
+        if (m[i] > 0 && !dp[i]) {
+            for (j = v; j >= 0; --j) {
+                if (!dp[j] || dp[sum])
+                    continue;
+                for (k = 1; k < m[i]; ++k) {
+                    if (j + k * i == sum) {
+                        dp[j + k * i] = 1;
+                        break;
+                    }
+                    if (j + k * i > sum || dp[j + k * i])
+                        break;
+                    dp[j + k * i] = 1;
+                }
+            }
+        }
+        v += i * m[i];
+        if (v > sum)
+            v = sum;
     }
 
-    return dp[v] >= 0;
+    return dp[v];
 }
